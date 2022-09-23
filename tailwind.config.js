7661ed7e6781ba59cd57e1b7b35d381e11d8667f
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-09-23 14:44:43 +0200

    installation complète webpack encore et tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // "./assets/**/*.js",
+    "./templates/article/add.html.twig",
+    "./templates/article/article.html.twig",
+    "./templates/article/articles.html.twig",
+    "./templates/login/index.html.twig",
+    "./templates/profil/update.html.twig",
+    "./templates/registration/register.html.twig",
+    "./templates/base.html.twig",
+  ],
   theme: {
     extend: {},
   },
